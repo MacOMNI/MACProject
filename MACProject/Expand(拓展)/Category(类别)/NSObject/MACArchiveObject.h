4e0fba9468e7a7c227225aa54d0a67be3c4e934f
azheng51714 <414294494@qq.com>
2016-12-29 16:19:23 +0800

    ArchiveObject

---
+++ MACProject/Expand(拓展)/Category(类别)/NSObject/MACArchiveObject.h
@@ -0,0 +1,13 @@
+//
+//  MACArchiveObject.h
+//  MACProject
+//
+//  Created by MacKun on 2016/12/29.
+//  Copyright © 2016年 com.mackun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MACArchiveObject :NSObject<NSCoding>
+
+@end
---
+++ MACProject/Expand(拓展)/Category(类别)/NSObject/MACArchiveObject.m
@@ -0,0 +1,55 @@
+//
+//  MACArchiveObject.m
+//  MACProject
+//
+//  Created by MacKun on 2016/12/29.
+//  Copyright © 2016年 com.mackun. All rights reserved.
+//
+
+#import "MACArchiveObject.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+@implementation MACArchiveObject
+-(instancetype)initWithCoder:(NSCoder *)aDecoder{//解档
+    
+    if (self = [super init]) {
+        
+        unsigned int ivarCount = 0;
+        Ivar *ivarList = class_copyIvarList(self.class, &ivarCount);
+        
+        for (int i = 0; i < ivarCount; i++) {
+            Ivar ivar = ivarList[i];
+            const char *key = ivar_getName(ivar);
+            NSString *_key = [NSString stringWithUTF8String:key];
+            if (_key) {
+                id value = [aDecoder decodeObjectForKey:_key];//解档取值
+                [self setValue:value forKey:_key];
+            }
+        }
+        
+        free(ivarList);
+    }
+    
+    return self;
+}
+
+-(void)encodeWithCoder:(NSCoder *)aCoder{//归档
+    
+    unsigned int ivarCount = 0;
+    Ivar *ivarList = class_copyIvarList(self.class, &ivarCount);
+    
+    for (int i = 0; i < ivarCount; i++) {
+        Ivar ivar = ivarList[i];
+        const char *key = ivar_getName(ivar);
+        NSString *_key = [NSString stringWithUTF8String:key];
+        
+        if (_key) {
+            id value = [self valueForKey:_key];
+            [aCoder encodeObject:value forKey:_key];
+            
+        }
+    }
+    
+}
+
+@end
